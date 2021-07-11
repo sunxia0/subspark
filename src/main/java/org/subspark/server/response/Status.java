1c2c5dd61af9332ece79e63a8b2c60f8da6a07eb
Noah <873717994@qq.com>
2021-07-11 22:54:05 +0800

    refactor: add two response status in `Status`
    add `401 Unauthorized` and `405 Method Not Allowed` in `Status`

--- src/main/java/org/subspark/server/response/Status.java
+++ src/main/java/org/subspark/server/response/Status.java
@@ -6,8 +6,10 @@ public enum Status {
     OK(200, "OK"),
 
     BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
     FORBIDDEN(403, "Forbidden"),
     NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
 
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
     NOT_IMPLEMENTED(501, "Not Implemented");
