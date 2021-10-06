ffd3056f3e8d24c34ed0da073d0a4516f9f413ac
Noah <873717994@qq.com>
2021-10-07 00:13:21 +0800

    build: reorganize `pom.xml` and `log4j2.yaml`
    - remove redundant plugins in `pom.xml`
    - add file appender in log configuration

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -5,21 +5,18 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.subspark.http.Status;
 
-import java.util.List;
-import java.util.Map;
-
 public class WebServer {
     final static Logger logger = LogManager.getLogger(WebServer.class);
 
     public static void main(String[] args) {
         org.apache.logging.log4j.core.config.Configurator.setLevel("org.subspark", Level.DEBUG);
 
-        post("/test1", (req, res) -> {
-            res.redirect("/test2", Status.TEMPORARY_REDIRECT);
+        get("/test1", (req, res) -> {
+            res.redirect("/test2", Status.MOVED_PERMANENTLY);
             return null;
         });
 
-        post("/test2", (req, res) -> {
+        get("/test2", (req, res) -> {
             return "post /test2";
         });
 
